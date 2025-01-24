9f236751b39d32929a7acb84ce1b3c3ef4d3bfb8
Le190cm <salvadorkbel@gmail.com>
2025-01-24 10:38:16 +0100

    modifications bouton paarticipants

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import {
   CallControls,
-  CallParticipantsList,
   CallStatsButton,
   CallingState,
   PaginatedGridLayout,
@@ -10,7 +9,7 @@ import {
   useCallStateHooks,
 } from '@stream-io/video-react-sdk';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Users, LayoutList } from 'lucide-react';
+import { LayoutList } from 'lucide-react';
 
 import {
   DropdownMenu,
@@ -30,7 +29,6 @@ const MeetingRoom = () => {
   const isPersonalRoom = !!searchParams.get('personal');
   const router = useRouter();
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
-  const [showParticipants, setShowParticipants] = useState(false);
   const { useCallCallingState } = useCallStateHooks();
 
   // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
@@ -54,13 +52,6 @@ const MeetingRoom = () => {
       <div className="relative flex size-full items-center justify-center">
         <div className=" flex size-full max-w-[1000px] items-center">
           <CallLayout />
-        </div>
-        <div
-          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
-            'show-block': showParticipants,
-          })}
-        >
-          <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
       {/* video layout and call controls */}
@@ -89,11 +80,7 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
-        <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
-            <Users size={20} className="text-white" />
-          </div>
-        </button>
+        {/* Le bouton pour afficher les participants*/}
         {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
